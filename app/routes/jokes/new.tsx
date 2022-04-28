b92a2dc72ed767aef420a0a18e1f09714be1d3d9
augusto.terra <augusto.terra@ifood.com.br>
2022-04-28 15:12:21 -0300

    fix: changes import type

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,4 +1,5 @@
-import { ActionFunction, json } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { useActionData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
